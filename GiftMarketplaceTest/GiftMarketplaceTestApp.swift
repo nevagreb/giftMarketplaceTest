053c9107d304dc8213eb1721c5dae8d5da293982
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2025-09-04 12:28:45 +0200

    Initial Commit

---
+++ GiftMarketplaceTest/GiftMarketplaceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftMarketplaceTestApp.swift
+//  GiftMarketplaceTest
+//
+//  Created by Kristina Grebneva on 04.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GiftMarketplaceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
